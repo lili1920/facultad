48ea1b785ab619348275c6e97cd98c11285b1c9c
lili1920 <you@example.com>
2020-12-26 16:44:54 -0300

    subo los scripts de materia User_materia

---
+++ script_sqls/materia.sql
@@ -0,0 +1,8 @@
+use facultad;
+
+create table materia (id int (30) auto_increment primary key, 
+   description varchar(20)
+   );
+drop table materia;
+
+select * from materia;
---
+++ script_sqls/user_materia.sql
@@ -0,0 +1,7 @@
+use facultad;
+
+create table user_materia (id int auto_increment primary key, id_user int (10) not null,
+   id_materia int (30)not null
+   );
+
+select * from user_materia;
